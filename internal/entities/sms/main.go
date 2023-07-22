95d20b41315d8ef59fd3a547a7d54d2ac6964d9d
VictorMarchenkov <VictorMarchenkov@users.noreply.github.com>
2023-07-22 09:33:55 +0300

    refactor entities

--- internal/entities/sms/main.go
+++ internal/entities/sms/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-<<<<<<< HEAD
 	"sb-diplom-v2/internal/entities"
 	"strconv"
 	"strings"
@@ -44,57 +43,12 @@ func newFromString(str string) (Data, error) {
 		return Data{}, fmt.Errorf("wrong number of fields: %d", len(fields))
 	}
 	result := Data{
-=======
-	"strconv"
-	"strings"
-
-	"sb-diplom-v2/internal/entities"
-)
-
-type SMSData struct {
-	country         string
-	throughput      uint8
-	avgResponseTime uint
-	provider        string
-}
-
-func (s *SMSData) validate() error {
-	if len(s.country) != 2 {
-		return fmt.Errorf("invalid country code length: %v", s.country)
-	}
-
-	if _, ok := entities.ISOCountries[s.country]; !ok {
-		return fmt.Errorf("unknown country code: %v", s.country)
-	}
-
-	if s.provider == "" {
-		return fmt.Errorf("provider is empty")
-	}
-
-	if _, ok := entities.Providers[s.provider]; !ok {
-		return fmt.Errorf("unknown provider: %v", s.provider)
-	}
-
-	return nil
-}
-
-type SMSRecords []SMSData
-
-func newFromString(str string) (SMSData, error) {
-	fields := strings.Split(str, ";")
-	if len(fields) != 4 {
-		return SMSData{}, fmt.Errorf("wrong number of fields: %d", len(fields))
-	}
-
-	result := SMSData{
->>>>>>> origin/master
 		country:  fields[0],
 		provider: fields[3],
 	}
 
 	v, err := strconv.ParseUint(fields[1], 10, 8)
 	if err != nil {
-<<<<<<< HEAD
 		return Data{}, fmt.Errorf("invalid throughput: %q", fields[1])
 	}
 	result.bandWidth = uint8(v)
@@ -107,41 +61,19 @@ func newFromString(str string) (SMSData, error) {
 
 	if err := result.validate(); err != nil {
 		return Data{}, err
-=======
-		return SMSData{}, fmt.Errorf("invalid throughput: %q", fields[1])
-	}
-	result.throughput = uint8(v)
-
-	v, err = strconv.ParseUint(fields[2], 10, 64)
-	if err != nil {
-		return SMSData{}, fmt.Errorf("invalid response time: %q", fields[2])
-	}
-	result.avgResponseTime = uint(v)
-
-	if err := result.validate(); err != nil {
-		return SMSData{}, err
->>>>>>> origin/master
 	}
 
 	return result, nil
 }
 
-<<<<<<< HEAD
 func NewFromFile(fileName string) (Records, error) {
-=======
-func NewFromFile(fileName string) (SMSRecords, error) {
->>>>>>> origin/master
 	file, err := os.Open(fileName)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-<<<<<<< HEAD
 	var result Records
-=======
-	var result SMSRecords
->>>>>>> origin/master
 	var line string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
